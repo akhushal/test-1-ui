53d0bab5e21761c9ebc166a89500f8faf5f52bfe
akhushal <khushal0042@gmail.com>
2025-04-26 13:06:53 +0530

    dual languages test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import './i18n'; // 👈 import the config
+import { useTranslation } from 'react-i18next';
 
 function App() {
   const[data, setData] = useState();
+  const { t, i18n } = useTranslation();
+
+  const switchLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
   const clickedButton = async () => {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
@@ -25,6 +32,11 @@ function App() {
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
       )}
+      <h1>{t('welcome')}</h1>
+      <p>{t('description')}</p>
+
+      <button onClick={() => switchLanguage('en')}>English</button>
+      <button onClick={() => switchLanguage('hi')}>हिंदी</button>
     </div>
   );
 }
